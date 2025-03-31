366d232caef5fa9076c5593d91353002823eb36d
dulat01 <mdulat2004@mail.ru>
2025-03-31 20:28:25 +0500

    7

--- app/src/main/java/com/example/lab7/SignUpActivity.kt
+++ app/src/main/java/com/example/lab7/SignUpActivity.kt
@@ -1,47 +1,41 @@
-package com.example.lab7;
+package com.example.lab7
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
-public class SignUpActivity extends AppCompatActivity {
-    private EditText edUsername;
-    private EditText edPassword;
-    private EditText edConfirmPassowrd;
-    private Button btnCreateUser;
-    private final String CREDENTIAL_SHARED_PREF = "our_shared_pref";
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var edUsername: EditText
+    private lateinit var edPassword: EditText
+    private lateinit var edConfirmPassword: EditText
+    private lateinit var btnCreateUser: Button
+    private val CREDENTIAL_SHARED_PREF = "our_shared_pref"
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
 
-        edUsername = findViewById(R.id.ed_username);
-        edPassword = findViewById(R.id.ed_password);
-        edConfirmPassowrd = findViewById(R.id.ed_confirm_pwd);
-        btnCreateUser = findViewById(R.id.btn_create_user);
+        edUsername = findViewById(R.id.ed_username)
+        edPassword = findViewById(R.id.ed_password)
+        edConfirmPassword = findViewById(R.id.ed_confirm_pwd)
+        btnCreateUser = findViewById(R.id.btn_create_user)
 
-        btnCreateUser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String strPassword = edPassword.getText().toString();
-                String strConfirmPassword = edConfirmPassowrd.getText().toString();
-                String strUsername = edUsername.getText().toString();
+        btnCreateUser.setOnClickListener {
+            val strPassword = edPassword.text.toString()
+            val strConfirmPassword = edConfirmPassword.text.toString()
+            val strUsername = edUsername.text.toString()
 
-                if (strPassword != null && strConfirmPassword != null && strPassword.equalsIgnoreCase(strConfirmPassword)) {
-                    SharedPreferences credentials = getSharedPreferences(CREDENTIAL_SHARED_PREF, Context.MODE_PRIVATE);
-                    SharedPreferences.Editor editor = credentials.edit();
-                    editor.putString("Password", strPassword);
-                    editor.putString("Username", strUsername);
-                    editor.commit();
+            if (strPassword.isNotEmpty() && strConfirmPassword.isNotEmpty() && strPassword.equals(strConfirmPassword, ignoreCase = true)) {
+                val credentials = getSharedPreferences(CREDENTIAL_SHARED_PREF, Context.MODE_PRIVATE)
+                val editor = credentials.edit()
+                editor.putString("Password", strPassword)
+                editor.putString("Username", strUsername)
+                editor.commit()
 
-                    SignUpActivity.this.finish();
-                }
+                finish()
             }
-        });
+        }
     }
 } 
